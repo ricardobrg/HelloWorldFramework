665d7a31eed14138183895874bc76fe7f2454728
Ricardo Gonçalves <ricardo@mytiki.com>
2022-11-03 12:38:52 -0300

    initial

---
+++ HelloWorldFramework/HelloWorld.swift
@@ -0,0 +1,14 @@
+//
+//  HelloWorld.swift
+//  HelloWorldFramework
+//
+//  Created by Ricardo on 03/11/22.
+//
+
+import Foundation
+
+class HelloWorld{
+    func say(){
+        print("Hello World")
+    }
+}
---
+++ HelloWorldFramework/HelloWorldFramework.h
@@ -0,0 +1,18 @@
+//
+//  HelloWorldFramework.h
+//  HelloWorldFramework
+//
+//  Created by Ricardo on 03/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloWorldFramework.
+FOUNDATION_EXPORT double HelloWorldFrameworkVersionNumber;
+
+//! Project version string for HelloWorldFramework.
+FOUNDATION_EXPORT const unsigned char HelloWorldFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloWorldFramework/PublicHeader.h>
+
+
